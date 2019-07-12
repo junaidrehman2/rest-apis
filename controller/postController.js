a4f7c5651792abe6e0f55186e2d4411d2518f9c0
JunaidRehman <junaid@homepass.com>
2019-07-12 15:42:01 +1000

    added validation

--- controller/postController.js
+++ controller/postController.js
@@ -1,3 +1,14 @@
+const validationHandler = require("../validations/validationHandler");
+
 exports.index = (req, res) => {
   res.send({ message: "Hi" });
+};
+
+exports.store = (req, res, next) => {
+  try {
+    validationHandler(req);
+    res.send({ message: "the name is" + req.body.name });
+  } catch (err) {
+    next(err);
+  }
 };
