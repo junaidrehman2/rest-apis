d576270610d244e12220d730c16ee831c7a08791
JunaidRehman <junaid@homepass.com>
2019-07-16 16:45:17 +1000

    added a post route and setup mongo

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express");
 // const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mongoose = require("mongoose");
 const postRoutes = require("./routes/post");
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 // app.use(cors);
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/rest-api-node", {
+  useNewUrlParser: true
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 const port = process.env.PORT || 3100;
 app.use("/api/post", postRoutes);
@@ -19,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("welcome..d.");
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
